f05777fd93339c6a51bede50d8bf8657ead9ffb6
Marco Lombardini <marco.lombardini@gmail.com>
2019-03-06 15:16:24 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,4 +1,3 @@
-
 def levitation_quiz
     loop do 
         puts "What is the spell that enacts levitation?"
@@ -7,7 +6,3 @@ def levitation_quiz
     end
     puts "You passed the quiz!"
 end
-
-levitation_quiz
-
-
